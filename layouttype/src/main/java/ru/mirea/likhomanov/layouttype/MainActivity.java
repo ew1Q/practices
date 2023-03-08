673847e1c9d9b0a7a0c6f54903a82c32eee016e2
aleksey <121677137+ew1Q@users.noreply.github.com>
2023-03-08 15:10:30 +0300

    +not

--- layouttype/src/main/java/ru/mirea/likhomanov/layouttype/MainActivity.java
+++ layouttype/src/main/java/ru/mirea/likhomanov/layouttype/MainActivity.java
@@ -3,12 +3,21 @@ package ru.mirea.likhomanov.layouttype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.table_layout);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button8);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
     }
 }
